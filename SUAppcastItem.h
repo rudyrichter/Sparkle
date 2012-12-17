ff8d0458a0843f70e04cfb741677e4a8f45e4ad1
Rudy Richter <rudy@growl.info>
2012-12-17 11:39:46 -0500

    changes for compilation's sake

--- SUAppcastItem.h
+++ SUAppcastItem.h
@@ -33,8 +33,8 @@
 }
 
 // Initializes with data from a dictionary provided by the RSS class.
-- initWithDictionary:(NSDictionary *)dict;
-- initWithDictionary:(NSDictionary *)dict failureReason:(NSString**)error;
+- (id)initWithDictionary:(NSDictionary *)dict;
+- (id)initWithDictionary:(NSDictionary *)dict failureReason:(NSString**)error;
 
 - (NSString *)title;
 - (NSString *)versionString;
